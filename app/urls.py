8ab4af7f4bf68ff070fab4149f734e13e2f41230
Ruhshan <ruhshan.ahmed@gmail.com>
2019-04-21 00:25:47 -0400

    pdf generation enabled

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PageView, InvoiceView
+from .views import PageView, InvoiceView, InvoicePdfView
 from rest_framework import routers
 from .viewsets import OutletViewSet, ProductViewSet, StockViewSet, InvoiceViewSet
 
 urlpatterns = [
     path('', PageView.as_view()),
-    path('invoice',InvoiceView.as_view(), name='invoice-view')
+    path('invoice',InvoiceView.as_view(), name='invoice-view'),
+    path('invoice_pdf/<int:invoice_id>',InvoicePdfView)
 
 ]
 router = routers.SimpleRouter()
--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponse
+from weasyprint import HTML, CSS
 from django.shortcuts import render
+from django.db.models import F, ExpressionWrapper, FloatField, Sum
+from django.template.loader import get_template
+from django.conf import settings
 
+from .models import Invoice
 # Create your views here.
 
 from django.views.generic import TemplateView
@@ -11,3 +17,22 @@ class PageView(TemplateView):
 
 class InvoiceView(TemplateView):
     template_name = "app/Invoice.html"
+
+
+def InvoicePdfView(request,invoice_id):
+    if request.method=="GET":
+        invoice = Invoice.objects.get(invoice_id=invoice_id)
+        products = invoice.invoiceproduct_set.annotate(amount=ExpressionWrapper(F("quantity")*F("product_number__price"),output_field=FloatField())).all()
+        total_quantity = products.aggregate(Sum('quantity'))["quantity__sum"]
+        total_amount = products.aggregate(Sum('amount'))["amount__sum"]
+
+        html_template = get_template('app/_invoice_pdf.html').render(
+            {'invoice':invoice, 'products':products,"total_quantity":total_quantity,"total_amount":total_amount},
+            request)
+        pdf_file = HTML(string=html_template, base_url=request.build_absolute_uri()).write_pdf()
+        response = HttpResponse(pdf_file, content_type='application/pdf')
+        response['Content-Disposition'] = 'filename="home_page.pdf"'
+
+        return response
+        #return render(request, "app/_invoice_pdf.html",{'invoice':invoice, 'products':products,"total_quantity":total_quantity,"total_amount":total_amount})
+
