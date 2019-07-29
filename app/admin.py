580383508d1fb2efd8c2caada58ff0297138af27
Ruhshan <ruhshan.ahmed@gmail.com>
2019-07-29 22:15:25 +0600

    added product in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Outlet, Stock, Invoice, InvoiceProduct
+from app.models import Outlet, Stock, Invoice, InvoiceProduct, Product
 
 
 admin.site.register(Outlet)
 admin.site.register(Stock)
 admin.site.register(Invoice)
 admin.site.register(InvoiceProduct)
+admin.site.register(Product)
